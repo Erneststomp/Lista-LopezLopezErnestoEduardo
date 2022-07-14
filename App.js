830bfe5c9e016df1646fefedc43b292148d7d0f1
ErnestStomp <ernesto.lopez.lbm@outlook.com>
2022-07-13 20:02:55 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, Text, View , Button, FlatList, TouchableOpacity, Modal} from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 export default function App() {
+  const [textItem,setTextItem]=useState('');
+  const [itemList,setItemList]=useState([]);
+  const [isSelected, setSelection] = useState(false);
+  const[modalVisible,setModalVisible]=useState(false);
+  const[itemSelected,setItemSelected]=useState({});
+
+  const onHndlerChangeItem=(text)=>setTextItem(text)
+  const onHndlerAddItem=()=>{
+    setItemList(currentItems=>[...currentItems,{id:itemList.length,value:textItem}])
+    setTextItem('')
+  }
+  const onHndlerDeleteItem=id=>{
+    setItemList(currentItems=>currentItems.filter(item=>item.id !==id))
+    setItemSelected({});
+    setModalVisible(!modalVisible)
+  }
+  const onHndlerModal=id=>{
+    setItemSelected(itemList.find(item=>item.id===id))
+    setModalVisible(!modalVisible)
+  }
+
+
+
+
   return (
+    
     <View style={styles.container}>
-      <Text>Hola Coder</Text>
-      <Text>Wiiiii!!!!</Text>
-      <StatusBar style="auto" />
+      <Modal animationType='slide' transparent={true} visible={modalVisible}>
+        <View>
+          <Text>Modal</Text>
+        </View>
+        <View>
+          <Text>
+            Estas seguro de que deseas borrar?
+          </Text>
+        </View>
+        <View>
+          <Text>{itemSelected.value}</Text>
+        </View>
+        <View>
+          <Button onPress={()=>onHndlerDeleteItem(itemSelected.id)} title='Confirmar'/>
+        </View>
+      </Modal>
+
+      <View style={styles.header}>
+        <TextInput style={styles.textinput} placeholder='Agregar Tarea' value={textItem} onChangeText={onHndlerChangeItem} />
+        <Button style={styles.buttonadd} title='Add' onPress={onHndlerAddItem} disabled={textItem.length==0?true:false}/>
+      </View>
+      <FlatList style={styles.flat}
+        data={itemList}
+        renderItem={data=>(
+          <TouchableOpacity onPress={()=>onHndlerModal(data.item.id)}>
+            <View style={styles.textdeployer}>  
+              <BouncyCheckbox value={isSelected} onValueChange={setSelection} style={styles.checkbox}/> 
+                <Text style={styles.textdeploy}>
+                  <Text style={styles.textout}>{data.item.value} </Text>
+                </Text>
+            </View>
+            
+          </TouchableOpacity>
+        )}
+        keyExtractor={item=> item.id}
+      />
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width:'100%',
+    marginTop:'10%',
+    paddingLeft:'5%',
+    paddingRight:'5%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width:'95%',
+    marginTop:'10%',
+    paddingLeft:'5%',
+    paddingRight:'5%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:'red',
+    backgroundColor: "aliceblue",
+
+  },
+  textinput:{
+  width:'80%',
+  },
+  textout:{
+    width:'80%',
+    },
+  buttonadd:{
+    width:'20%',
+    paddingRight:'0px',
+    marginEnd:'0px',
+  },
+  flat:{
+    paddingTop:'1%',
+    width:'95%',
+    height:'90%',
+  },
+  textdeploy:{
+    width:'80%',
+  },
+  textdeployer:{
+    marginTop:'5%',
+    width:'95%',
+    backgroundColor: "aliceblue",
+    alignItems:'center',
+    justifyContent:'space-between',
+    flexDirection:'row',
+  }, 
+  checkbox:{
+    paddingLeft:'2%',
+    fillColor:"red",
+    unfillColor:"#FFFFFF"
+  }
 });
